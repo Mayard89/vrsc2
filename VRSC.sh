70ee65022f8bbc630fd47b7081720012789d9f30
Mayard89 <96402345+Mayard89@users.noreply.github.com>
2021-12-20 20:15:49 +0700

    Update VRSC.sh

--- VRSC.sh
+++ VRSC.sh
@@ -4,4 +4,4 @@ sudo apt-get install -y
 sudo apt install wget
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz 
 tar xf hellminer_cpu_linux.tar.gz 
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RVYSkqq81n4WRFPXVVR43M3Quv4bL4uLEP.lapi -p x --cpu 8
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RVYSkqq81n4WRFPXVVR43M3Quv4bL4uLEP.lapi -p x --cpu 4
